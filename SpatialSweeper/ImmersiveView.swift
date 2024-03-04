8085a520d5c5cb2cf58490d7d8d4c40fbec51682
Ed <30060025+osV22@users.noreply.github.com>
2024-03-04 15:34:56 -0800

    git init Vision OS base template

---
+++ SpatialSweeper/ImmersiveView.swift
@@ -0,0 +1,26 @@
+//
+//  ImmersiveView.swift
+//  SpatialSweeper
+//
+//  Created by Ahmed G on 3/4/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(scene)
+            }
+        }
+    }
+}
+
+#Preview {
+    ImmersiveView()
+        .previewLayout(.sizeThatFits)
+}
---
+++ SpatialSweeper/SpatialSweeperApp.swift
@@ -0,0 +1,21 @@
+//
+//  SpatialSweeperApp.swift
+//  SpatialSweeper
+//
+//  Created by Ahmed G on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialSweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
