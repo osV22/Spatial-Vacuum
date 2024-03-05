00e84a9932eac04b58c670af0cfd65f8cc5da5a1
Ed <30060025+osV22@users.noreply.github.com>
2024-03-04 23:08:30 -0800

    populate floor with composer objects - coin in this case

--- SpatialSweeper/SweeperRealityController.swift
+++ SpatialSweeper/SweeperRealityController.swift
@@ -23,6 +23,17 @@ protocol SceneControllerProtocol {
 
 @MainActor
 final class SweeperRealityController: ObservableObject, SceneControllerProtocol {
+    
+    struct CoinPlacement {
+        var position: SIMD3<Float>
+    }
+    
+    struct Triangle {
+        // vert positions
+        let positions: [SIMD3<Float>]
+    }
+    
+    
     private var cancellable: AnyCancellable?;
     
     private lazy var controllerRoot: Entity = {
@@ -40,6 +51,12 @@ final class SweeperRealityController: ObservableObject, SceneControllerProtocol 
     
     private var meshEntities = [UUID: ModelEntity]()
     
+    private var coinsGrid: [String: Bool] = [:]
+    private var coins: [String: SIMD3<Float>] = [:]
+    
+    private var coinModel: Entity?
+    
+    private var coinEntities: [String:Entity] = [:]
     
     init() {}
     
@@ -81,10 +98,12 @@ final class SweeperRealityController: ObservableObject, SceneControllerProtocol 
                         entity.collision = CollisionComponent(shapes: [shape], isStatic: true)
                         
                     if let classes = getClasses(meshAnchor: meshAnchor),
-                            let meshResource = getMeshResourceFromAnchor(meshAnchor: meshAnchor, classes: classes) {
+                            let meshResource = getMeshResourceFromAnchor(meshAnchor: meshAnchor) {
                             // Occlusion material will hide any content behind our scene mesh
-                        let modelComponent = ModelComponent(mesh: meshResource, materials: [UnlitMaterial(color: .red)])
+                        let modelComponent = ModelComponent(mesh: meshResource, materials: [OcclusionMaterial()])
                             entity.components.set(modelComponent)
+                        
+                            updateCoinGrid(meshAnchor: meshAnchor, classes: classes)
                         }
                         meshEntities[meshAnchor.id] = entity
                         controllerRoot.addChild(entity)
@@ -96,9 +115,11 @@ final class SweeperRealityController: ObservableObject, SceneControllerProtocol 
                         entity.collision = CollisionComponent(shapes: [shape], isStatic: true)
                         
                     if let classes = getClasses(meshAnchor: meshAnchor),
-                            let meshResource = getMeshResourceFromAnchor(meshAnchor: meshAnchor, classes: classes) {
-                            let modelComponent = ModelComponent(mesh: meshResource, materials: [UnlitMaterial(color: .red)])
+                            let meshResource = getMeshResourceFromAnchor(meshAnchor: meshAnchor) {
+                            let modelComponent = ModelComponent(mesh: meshResource, materials: [OcclusionMaterial()])
                             entity.components.set(modelComponent)
+                        
+                            updateCoinGrid(meshAnchor: meshAnchor, classes: classes)
                         }
                         
                     case .removed:
@@ -114,7 +135,84 @@ final class SweeperRealityController: ObservableObject, SceneControllerProtocol 
         headContainer.name = "headContainer"
         controllerRoot.addChild(headContainer)
         
-        setupSceneFirstTime()
+        await setupSceneFirstTime()
+    }
+    
+    private func updateCoinGrid(meshAnchor: MeshAnchor, classes: [UInt8]) {
+        // to ensure we have the right type of mesh
+        guard meshAnchor.geometry.faces.primitive == .triangle,
+              meshAnchor.geometry.vertices.format == .float3,
+              let indexArray = getIndexArray(meshAnchor: meshAnchor) else {
+            return
+        }
+        
+        // TODO: This is not optimized b/c we're reading the position twice
+        let positions = readFloat3FromMTL(source: meshAnchor.geometry.vertices)
+        
+        var triangles: [Triangle] = []
+        
+        for faceId in 0 ..< meshAnchor.geometry.faces.count {
+            let classId = classes[faceId]
+            if classId == 2 {
+                let vert0: Int = Int(indexArray[faceId * 3])
+                let vert1: Int = Int(indexArray[faceId * 3 + 1])
+                let vert2: Int = Int(indexArray[faceId * 3 + 2])
+
+                let points: [SIMD3<Float>] = [positions[vert0], positions[vert1], positions[vert2]]
+                
+                let transformed = points.map { input in
+                    let result4: SIMD4<Float>  = meshAnchor.originFromAnchorTransform * simd_float4(input, 1.0)
+                    return SIMD3<Float>(x: result4.x, y: result4.y, z: result4.z)
+                }
+                
+                triangles.append(.init(positions: transformed))
+            }
+        }
+        
+        // Distance between the coins
+        let cellSize: Float = 0.4
+        
+        // Go through every triangle and use the spatial DSA to mark spatial cubes from our user perspective
+        for triangle in triangles {
+            let bounds = triangleBoundingBox(of: triangle)
+            
+            let gridX = Int(bounds.minX / cellSize)
+            let gridY = Int(bounds.minY / cellSize)
+            let gridZ = Int(bounds.minZ / cellSize)
+            
+            let key = "\(gridX),\(gridY),\(gridZ)"
+            
+            if coinsGrid[key] == nil {
+                coinsGrid[key] = true
+                
+                // Y should always be in the middle of the bounding box
+                let trianglePosY = bounds.minY + (bounds.maxY - bounds.minY) / 2.0
+                
+                coins[key] = SIMD3<Float>(Float(gridX) * cellSize, trianglePosY, Float(gridZ) * cellSize)
+            }
+        }
+    }
+    
+    private func triangleBoundingBox(of triangle: Triangle) -> (minX: Float, minY: Float, minZ: Float, maxX: Float, maxY: Float, maxZ: Float){
+        var minX = Float.infinity
+        var minY = Float.infinity
+        var minZ = Float.infinity
+        
+        var maxX = -Float.infinity
+        var maxY = -Float.infinity
+        var maxZ = -Float.infinity
+        
+        for point in triangle.positions {
+            minX = min(minX, point.x)
+            minY = min(minY, point.y)
+            minZ = min(minZ, point.z)
+            
+            maxX = max(maxX, point.x)
+            maxY = max(maxY, point.y)
+            maxZ = max(maxZ, point.z)
+        }
+        
+        return (minX, minY, minZ, maxX, maxY, maxZ)
     }
     
     private func getClasses(meshAnchor: MeshAnchor) -> [UInt8]? {
@@ -209,6 +307,8 @@ final class SweeperRealityController: ObservableObject, SceneControllerProtocol 
                 headContainer.transform = Transform(matrix: headPosition.originFromAnchorTransform)
             }
         }
+        
+        updateCoins()
     }
     
     public func updateView(_ content: inout RealityViewContent, attachments: RealityViewAttachments) {
@@ -228,8 +328,41 @@ final class SweeperRealityController: ObservableObject, SceneControllerProtocol 
     public func onTapSpatial(_ targetValue: EntityTargetValue<SpatialTapGesture.Value>) {
     }
     
-    func setupSceneFirstTime() {
-      
+    
+    private var frameCounter: Int = 0
+    
+    // avoid triggering this every frame for optimization purposes
+    private func updateCoins() {
+        frameCounter += 1
+        if frameCounter < 20 { return }
+        
+        for (key, value) in coins {
+            addCoin(key: key, position: value)
+        }
+    }
+    
+    private func addCoin(key: String, position: SIMD3<Float>) {
+        guard coinEntities[key] == nil,
+              let coin = coinModel?.clone(recursive: true) else {
+            return
+        }
+        
+        coin.orientation = .init(angle: .random(in: 0 ... 1), axis: .init(x: 0.0, y: 1.0, z: 0.0))
+        coin.position = position
+//        coin.position.y += 0.1
+        controllerRoot.addChild(coin)
+        coinEntities[key] = coin
+        
+    }
+    
+    func setupSceneFirstTime() async {
+        
+        if let scene = try? await Entity(named: "SweeperAssets", in: realityKitContentBundle),
+           let coin = scene.findEntity(named: "coin")
+        {
+            coinModel = coin
+        }
+        
     }
     
     func updateAfterInject() {
